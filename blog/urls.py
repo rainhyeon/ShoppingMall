f7eeb853298572d3bb4fa6034fb04298e70575d8
Woo-Hyeon Lee <iiilee0907@naver.com>
2021-11-10 00:56:00 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [ # 서버IP/blog/
 
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),# 서버IP/blog/category/slug
