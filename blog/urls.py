1444f7c80155c91f7fe66bb6886de11cbbd844f4
rainhyeon <iiilee0907@naver.com>
2021-11-17 13:42:16 +0900

    폼으로 포스트 상세 페이지에 댓글 작성하기

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [ # 서버IP/blog/
     path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),# 서버IP/blog/category/slug
+    path('<int:pk>/new_comment/', views.new_comment),
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
