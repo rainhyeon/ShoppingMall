24dafc700b5d99d052635421e46f49d6714eeef1
rainhyeon <iiilee0907@naver.com>
2021-11-22 12:41:40 +0900

    검색 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 def new_comment(request, pk):
     if request.user.is_authenticated:
@@ -102,7 +103,6 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-
 class PostDetail(DetailView):
 
     model = Post
@@ -112,6 +112,23 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         context['comment_form'] = CommentForm
+        return context
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search : {q} ({self.get_queryset().count()})'
+
         return context
 
 def category_page(request, slug):
